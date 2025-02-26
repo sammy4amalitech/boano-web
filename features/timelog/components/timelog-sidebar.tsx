81fd32442e01cfa7e4513db2090880d651564531
sammy <aristocrat4gh@gmail.com>
2025-02-26 20:28:12 +0000

    revert back to tool sidebar

--- features/timelog/components/timelog-sidebar.tsx
+++ features/timelog/components/timelog-sidebar.tsx
@@ -3,25 +3,15 @@ import {
   SidebarContent,
 } from "@/components/ui/sidebar"
 import {useTimelogStore} from "@/lib/store";
-import {Timelogs, useTimelogs} from "@/features/timelog";
-import {TimelogEntry} from "@/types";
+import {Timelogs} from "@/features/timelog";
 
 export function TimelogSidebar() {
-    const { data: response } = useTimelogs();
-    console.log(response?.data)
-    const formattedTimelogs: TimelogEntry[] = response?.data?.data?.map(log => ({
-        title: `${log.task} - ${log.description}`,
-        date: new Date(log.created_at).toLocaleDateString(),
-        start_time: new Date(log.start_time).toLocaleTimeString(),
-        end_time: log.end_time ? new Date(log.end_time).toLocaleTimeString() : '',
-        source: log.source
-    })) || [];
-
-    return (
-        <Sidebar side="right" >
-            <SidebarContent>
-                <Timelogs timelogs={formattedTimelogs}/>
-            </SidebarContent>
-        </Sidebar>
-    )
+  const {timelogs} = useTimelogStore();
+  return (
+    <Sidebar side="right" >
+      <SidebarContent>
+        <Timelogs timelogs={timelogs}/>
+      </SidebarContent>
+    </Sidebar>
+  )
 }
